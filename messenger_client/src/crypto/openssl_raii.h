b01183f2486407c01100cf9477106d9e4e964219
ron1n101 <daticob@gmail.com>
2026-09-07 22:49:57 +0300

    Add unique_ptr wrappers for EVP_MD and EVP_MD_CTX to improve resource management

--- messenger_client/src/crypto/openssl_raii.h
+++ messenger_client/src/crypto/openssl_raii.h
@@ -15,6 +15,21 @@ namespace CryptoInternal {
         }
     };
 
+
+    struct EvpMdDeleter{
+        void operator()(EVP_MD *p) const {
+            EVP_MD_free(p);
+        }
+    };
+
+    struct EvpMdCtxDeleter{
+        void operator()(EVP_MD_CTX *p) const {
+            EVP_MD_CTX_free(p);
+        }
+    };
+
     using EvpPkeyPtr    = std::unique_ptr<EVP_PKEY, EvpPkeyDeleter>;
     using EvpPkeyCtxPtr = std::unique_ptr<EVP_PKEY_CTX, EvpPKeyCtxDeleter>;
+    using EvpMdPtr      = std::unique_ptr<EVP_MD, EvpMdDeleter>;
+    using EvpMdCtxPtr   = std::unique_ptr<EVP_MD_CTX, EvpMdCtxDeleter>;
 }
