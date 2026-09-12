7f1305d6a4f697e8c384dfb4b57b7aba30127664
ron1n101 <daticob@gmail.com>
2026-09-12 23:15:58 +0300

    Remove redundant bytesFromHex implementation from test_dh.cpp

--- messenger_client/tests/test_dh.cpp
+++ messenger_client/tests/test_dh.cpp
@@ -2,21 +2,7 @@
 #include <string>
 #include "crypto/diffie_helman.h"
 #include "crypto/crypto_exception.h"
-
-Bytes bytesFromHex (const std::string &hex)
-{
-    Bytes bytes;
-    bytes.reserve(hex.size() / 2); // 2 symbols per byte
-
-    for (size_t i = 0; i < hex.size(); i += 2)
-    {
-        std::string byteString = hex.substr(i, 2);
-        uint8_t byte = static_cast<uint8_t>(std::stoi(byteString, nullptr, 16)); // Convert hex string to byte (74)
-        bytes.push_back(byte);
-
-    }
-    return bytes;
-}
+#include "crypto/helper.h"
 
 
 TEST_CASE("bytesFromHex converts hex string correctly", "[helpers]")
